60b360d1abf33f1209d0c0d9cac9a65a6484aa43
Shohag-Rana <69742923+Shohag-Rana@users.noreply.github.com>
2022-03-16 12:36:54 +0600

    Updates

---
+++ precache-manifest.cb48215a8ef77c43bb6f1baab211c8ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "43f445407a1cd35fe218",
+    "url": "/myreactapp/static/css/main.9a4e07e3.chunk.css"
+  },
+  {
+    "revision": "43f445407a1cd35fe218",
+    "url": "/myreactapp/static/js/main.43f44540.chunk.js"
+  },
+  {
+    "revision": "db20f86d0530543db744",
+    "url": "/myreactapp/static/js/1.db20f86d.chunk.js"
+  },
+  {
+    "revision": "8766af2a480765e3343c",
+    "url": "/myreactapp/static/js/2.8766af2a.chunk.js"
+  },
+  {
+    "revision": "a4d5c58c091b5f13732c",
+    "url": "/myreactapp/static/js/runtime~main.a4d5c58c.js"
+  },
+  {
+    "revision": "e5d6733433c901d0179e7346803bc601",
+    "url": "/myreactapp/index.html"
+  }
+];
